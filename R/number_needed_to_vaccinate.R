a902e1471d3a8ef984c123300348842a31590082
Yohann Mansiaux <y.mansiaux@epiconcept.fr>
2025-10-21 15:42:04 +0200

    feat: add machadpo and tuite methods for nnv

--- R/number_needed_to_vaccinate.R
+++ R/number_needed_to_vaccinate.R
@@ -1,9 +1,38 @@
 #' Compute the number of individuals needed to vaccinate to prevent
-#' one event
-#' @param events Vector of number of events
+#' one event according to Machado et al. method
+#' @param events Vector of number of events observed
 #' @param events_averted Vector of number of events averted
 #' @param pop_at_risk Population at risk
 #' @param vaccine_effectiveness Vector of vaccine effectiveness
+#' @return The number of individuals needed to vaccinate to avert
+#' one event
+#' @details The number of individuals needed to vaccinate to prevent one event is calculated as described by Machado et al. (2019) <doi:10.2807/1560-7917.ES.2019.24.45.1900268>.
+#' @export
+#' @rdname vaccine_impact
+#' @examples
+#' events <- coverage_and_incidence_mock_data$incidence_data$events
+#' cumulative_coverage <- coverage_and_incidence_mock_data$coverage_data$cumulative_coverage
+#' events_averted <- compute_events_averted_by_vaccination(events,
+#' cumulative_coverage, vaccine_effectiveness)
+#' pop_at_risk <- 1000
+#' vaccine_effectiveness <- ve_mock_data$ve
+#' compute_number_needed_to_vaccinate_machado(events, events_averted, pop_at_risk, vaccine_effectiveness)
+compute_number_needed_to_vaccinate_machado <- function(
+  events,
+  events_averted,
+  pop_at_risk,
+  vaccine_effectiveness
+) {
+  Rb <- (events + events_averted) / pop_at_risk
+  NNv <- 1 / (Rb * vaccine_effectiveness)
+  NNv[is.infinite(NNv)] <- NA
+  return(NNv)
+}
+
+#' Compute the number of individuals needed to vaccinate to prevent
+#' one event according to Tuite and Fisman method
+#' @param number_of_vaccinated Vector of number of vaccinated
+#' @param events_averted Vector of number of events averted
 #' @return The number of individuals needed to vaccinate to avert
 #' one event
 #' @export
@@ -14,10 +43,8 @@
 #' cumulative_coverage <- coverage_and_incidence_mock_data$coverage_data$cumulative_coverage
 #' events_averted <- compute_events_averted_by_vaccination(events,
 #' cumulative_coverage, vaccine_effectiveness)
-#' pop_at_risk <- 1000
-#' vaccine_effectiveness <- ve_mock_data$ve
-#' compute_number_needed_to_vaccinate(events, events_averted, pop_at_risk, vaccine_effectiveness)
-compute_number_needed_to_vaccinate <- function(
+#' compute_number_needed_to_vaccinate_tuite(events, events_averted)
+compute_number_needed_to_vaccinate_tuite_fisman <- function(
   number_of_vaccinated,
   events_averted
 ) {
